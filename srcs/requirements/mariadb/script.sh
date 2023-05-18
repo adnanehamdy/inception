03985da67028f8e435b9f2a7f231185d9b113a90
okaabe <adnanhamdy222@gmail.com>
2023-05-18 21:09:53 +0100

    docker compose

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -2,9 +2,8 @@
 
 service mysql start
 echo bind-address= 0.0.0.0 >> /etc/mysql/my.cnf
- mysql -u root -p -e "CREATE USER '${ROOT}'@'localhost' IDENTIFIED BY '${ROOT_PASS}'";
- mysql -u root  -p -e "CREATE  USER '${USER}'@'localhost' IDENTIFIED BY '${USER_PASS}'";
- mysql -u root  -p  -e"GRANT ALL PRIVILEGES ON *.* TO '${USER}'@'localhost'";
- mysql -u root  -p -e "GRANT SELECT ,INSERT ,UPDATE ON *.* TO '${ROOT}'@'localhost'";
+ mysql -u root -p -e "CREATE DATABASE ${DB_NAME}"
+ mysql -u root  -p -e "CREATE  USER '${SQL_USER}'@'localhost' IDENTIFIED BY '${SQL_USER_PASS}'";
+ mysql -u root  -p  -e"GRANT ALL PRIVILEGES ON ${SQL_USER_PASS}.* TO '${USER}'@'localhost'";
  mysql -u root -p FLUSH PRIVILEGES;
 service mysql stop
