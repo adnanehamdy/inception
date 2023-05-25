de907bb7df8bd1eea0aa695d19684497e33eed47
okaabe <adnanhamdy222@gmail.com>
2023-05-25 18:19:20 +0100

    add wordpress volume

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 up:
-	/Applications/Docker.app/Contents/Resources/bin/docker compose -f srcs/docker-compose.yml up -d 
+	/Applications/Docker.app/Contents/Resources/bin/docker compose -f srcs/docker-compose.yml up
 down:
 	/Applications/Docker.app/Contents/Resources/bin/docker compose -f srcs/docker-compose.yml down
 
 restart: down up
 
 prune:
-	/Applications/Docker.app/Contents/Resources/bin/docker compose  -f srcs/docker-compose.yml rm --all
+	/Applications/Docker.app/Contents/Resources/bin/docker compose  -f srcs/docker-compose.yml rm -v  --all 
 	/Applications/Docker.app/Contents/Resources/bin/docker system prune --all
 # .PHONY: up down restart prune
--- srcs/requirements/wordpress/setup_wp.sh
+++ srcs/requirements/wordpress/setup_wp.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-cd /var/www/html/wordpress/
-wp core download --allow-root
+wp core download --allow-root --path="/var/www/html"
+sed -i 's/^listen = .*/listen = 9000/' /etc/php/7.3/fpm/pool.d/www.conf
 
-chown www-data:www-data /var/www/html/wordpress/
+mkdir -p /run/php
+# chown -R www-data:www-data /var/www/html
+# chmod -R 755 /var/www/html
+wp config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASS} --dbhost=mariadb --allow-root --path="/var/www/html"
+wp core install --url=${DOMAIN_NAME} --title="inception" --admin_name=${WP_ADMIN} --admin_password=${WP_PASS} --admin_email=${WP_MAIL} --allow-root --path="/var/www/html"
+# sed -i 's/^listen = .*/listen = 9000/' /etc/php/7.3/fpm/pool.d/www.conf
 
-wp config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASS} --allow-root
-wp core install --url=${DOMAIN_NAME} --title="inception" --admin_name=${WP_ADMIN} --admin_password=${WP_PASS} --admin-email=${WP_MAIL} --allow-root
-wp core config --dbhost=mariadb --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASS} --allow-root
-sed -i 's/^listen = .*/listen = wordpress:9000/' /etc/php/7.3/fpm/pool.d/www.conf
-
-# chmod +x wp-config.php
+php-fpm7.3  -F
